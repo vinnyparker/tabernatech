7c1ffebab7c072170349ab10fb7ccff34835c24b
vinnyparker <vinny.parker@gmail.com>
2022-03-01 20:23:24 -0300

    start project

---
+++ src/main/java/br/com/tabernatech/TabernatechApplication.java
@@ -0,0 +1,13 @@
+package br.com.tabernatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabernatechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabernatechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/tabernatech/TabernatechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tabernatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabernatechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
